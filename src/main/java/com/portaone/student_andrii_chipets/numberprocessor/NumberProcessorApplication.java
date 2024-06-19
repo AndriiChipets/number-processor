43f5f8c96f5d1fa7b5db8f35a3eb73c2504f9ee2
Andrii Chipets <andrii.chipets@gmail.com>
2024-06-19 11:25:42 +0300

    initial commit

---
+++ src/main/java/com/portaone/student_andrii_chipets/numberprocessor/NumberProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.portaone.student_andrii_chipets.numberprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumberProcessorApplication.class, args);
+    }
+}
---
+++ src/test/java/com/portaone/student_andrii_chipets/numberprocessor/NumberProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portaone.student_andrii_chipets.numberprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
