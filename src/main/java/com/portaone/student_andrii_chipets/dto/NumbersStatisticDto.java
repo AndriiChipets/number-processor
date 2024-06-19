5bbddcc3520aa31a8cb0a868ee2dd3a49aa84ee7
Andrii Chipets <andrii.chipets@gmail.com>
2024-06-19 14:13:38 +0300

    added NumbersStatisticDto entity and NumbersStatisticMapper to transfer the data from service layer to controller layer

---
+++ src/main/java/com/portaone/student_andrii_chipets/dto/NumbersStatisticDto.java
@@ -0,0 +1,18 @@
+package com.portaone.student_andrii_chipets.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder(setterPrefix = "with")
+@Data
+@AllArgsConstructor
+public class NumbersStatisticDto {
+    private String min;
+    private String max;
+    private String median;
+    private String average;
+    private String longestAscNumSequence;
+    private String longestDescNumSequence;
+    private String totalProcessingTime;
+}
---
+++ src/main/java/com/portaone/student_andrii_chipets/mapper/NumbersStatisticMapper.java
@@ -0,0 +1,20 @@
+package com.portaone.student_andrii_chipets.mapper;
+
+import com.portaone.student_andrii_chipets.domain.NumbersStatistic;
+import com.portaone.student_andrii_chipets.dto.NumbersStatisticDto;
+
+public class NumbersStatisticMapper {
+
+    public static NumbersStatisticDto mapToNumStatDto(NumbersStatistic ns) {
+
+        return NumbersStatisticDto.builder()
+                .withMin(String.valueOf(ns.getMin()))
+                .withMax(String.valueOf(ns.getMax()))
+                .withMedian(String.valueOf(ns.getMedian()))
+                .withAverage(String.valueOf(ns.getAverage()))
+                .withLongestAscNumSequence(String.valueOf(ns.getLongestAscNumSequence()))
+                .withLongestDescNumSequence(String.valueOf(ns.getLongestDescNumSequence()))
+                .withTotalProcessingTime(String.format("%.4f", ns.getTotalProcessingTime()))
+                .build();
+    }
+}
